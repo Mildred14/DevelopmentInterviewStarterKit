1f58c3abc732b2c8746a17780f69dfdedb183860
Nataly Silva <mildred_natali@hotmail.com>
2021-02-06 19:06:51 -0600

    send require data from people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,12 @@
 class PeopleController < ApplicationController
   def index
-    @people = Salesloft::Client.people
+    @people = Salesloft::Client.people['data'].map do |person|
+      [{
+        id: person['id'],
+        name: person['first_name'],
+        email: person['email_address'],
+        job: person['title'],
+      }]
+    end
   end
 end
